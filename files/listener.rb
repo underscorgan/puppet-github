ac9a1829376dbe405484329c1e16cf1955d5f33a
Adrien Thebo <adrien@puppetlabs.com>
2011-07-28 17:05:10 -0700

    Rewrote listener class.
    Added path compatibility for gitosis/gitolite, added authorization for
    module updating.

--- files/listener.rb
+++ files/listener.rb
@@ -3,21 +3,44 @@
 require 'rubygems'
 require 'sinatra'
 
-BASE_DIR = "/home/git/gitolite/repositories"
+class Listener < Sinatra::Base
 
-post '/update/:user/:repo' do
+  post '/update/:user/:repo' do
+    status = 404
+    allowed = {}
 
-  repo = "#{BASE_DIR}/#{params[:user]}/#{params[:repo]}.git"
+    # Parse out repositories we're allowed to update from github
+    begin
+      File.open("#{settings.basedir}/.github-allowed") do |file|
+        file.each do |line|
+          identifier, url = line.split(', ', 2)
+          allowed[identifier] = url
+        end
+      end
+    rescue => e
+      $stderr.puts "Error reading allowed github repos: #{e.backtrace.join('\n')}"
+      halt 403
+    end
 
-  if File.directory? repo
-    %x{git --git-dir #{repo} fetch --all --prune}
-    %x{git --git-dir #{repo} update-server-info --force}
-    200
-  else
+    repo_path = "#{settings.basedir}/#{params[:user]}-#{params[:repo]}.git"
+    identifier = "#{params[:user]}/#{params[:repo]}"
+    if File.directory? repo_path
+      if allowed.keys.include? identifier
+        cmd = %Q{git --git-dir #{repo_path} fetch #{allowed[identifier]} --prune}
+        $stderr.puts %Q{Updating repo #{identifier} with command #{cmd}"}
+        %x{#{cmd}}
+        status = 200
+      else
+        $stderr.puts "Disallowed repo #{repo_path}"
+      end
+    else
+      $stderr.puts "Non existent repo #{repo_path}"
+    end
+
+    status
+  end
+
+  not_found do
     404
   end
-end
-
-not_found do
-  404
 end
